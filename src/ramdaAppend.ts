d3d57f5b330d869e9aabab044e84472af3851a6e
Jarvis Yang <jarvisgyang@gmail.com>
2021-03-27 22:26:03 -0500

    Generics FTW and adding ramda.

---
+++ src/ramdaAppend.ts
@@ -0,0 +1,11 @@
+import * as R from 'ramda'
+
+const updatedArray = R.append<string>('baz', ['foo', 'bar']);
+
+console.log("updatedArray", updatedArray);
+
+const searchStr = 'foo'
+const hasSearchedString = R.any<string>((a: string) => R.contains(searchStr,[
+    'fooooo', 'bar', 'baz']))
+
+console.log('hasSearchedString', hasSearchedString);
